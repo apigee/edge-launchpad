2ae288be1c72a729410ad0f493123ccf34585a7b
Gautham V Kidiyoor <gauthamvk@gauthamvk-macbookpro.roam.corp.google.com>
2016-12-08 18:49:46 +0530

    orchestration working

--- baseAdapter.js
+++ baseAdapter.js
@@ -2,15 +2,13 @@
  * Created by Muthu on 28/11/16.
  */
 
-var manager                                 = require('./manager').getManager();
+baseAdapter = {
+    adapterContext: {},
 
-function baseAdapter() {
-    this.adapterContext                     = {};
-
-
-    function doTask(taskName,context,config,resourceName,subResourceName, params) {
+    doTask: function(taskName,context,config,resourceName,subResourceName, params) {
         //TODO:
 
+        /*
         var rName                           = config.configName;
         if (!this.adapterContext[rName]) {
             this.adapterContext[rName]      = {};
@@ -20,64 +18,66 @@ function baseAdapter() {
             this.prompt(context,config);
             this.adapterContext[rName].initialized = true;
         }
+        */
 
-                //TODO: check the taskName and call appropriate function
+        //TODO: check the taskName and call appropriate function
 
         switch (taskName.toUpperCase()) {
-            case 'CLEAN' : clean (context,config,resourceName,subResourceName,params); break;
-            case 'BUILD' : build (context,config,resourceName,subResourceName,params); break;
-            case 'DEPLOY': deploy(context,config,resourceName,subResourceName,params); break;
-            case 'PROMPT': prompt(context,config,resourceName,subResourceName,params); break;
+            case 'CLEAN' : this.clean (context,config,resourceName,subResourceName,params); break;
+            case 'BUILD' : this.build (context,config,resourceName,subResourceName,params); break;
+            case 'DEPLOY': this.deploy(context,config,resourceName,subResourceName,params); break;
+            case 'PROMPT': this.prompt(context,config,resourceName,subResourceName,params); break;
         }
 
-    }
+    },
 
 
     //do the same activity for all subresources/resources; don't override
-    function repeatForAll(taskName,context,config,resourceName, subResourceName, params ) {
+    gotoSubResources: function(taskName, context, config, resourceName, subResourceName, params ) {
         if (subResourceName) return;
 
-        var config = context.getConfig(resourceName);
+        var config = {}; //context.getConfig(resourceName);
 
         if (!config) return;
 
         if (config.subResources && config.subResources.length > 0) {
-
+            console.log('deploying all subresources')
             //use promise - synchronous invocation
             //loop through subresources
             //for each sub-resource get its adapter based on the resource & subResource type
             //config.configName = getName(resourceName, subResourceName);
             //call doTask on the subresource adapter with the subresource config. (set resourceType and resourceName attributes for the subresource config before passing, when getConfig() is not used
             //if there is an error then log the error (getLog().logErrorStatus(config, error))
+            this.gotoSubResources();
         } else if (config.resources && config.resources.length > 0) {
-            //use promise - synchronous invocation
-            //loop through subresources
-            //for each sub-resource get its adapter based on the resource & subResource type
-            //config.configName = getName(resourceName, subResourceName);
-            //call doTask on the subresource adapter with the subresource config. (set resourceType and resourceName attributes for the subresource config before passing, when getConfig() is not used
-            //if there is an error then log the error (getLog().logErrorStatus(config, error))
+            console.log('deploying all resources')
+            this.gotoSubResources();
         }
-    }
+    },
 
 
-    function clean(context,config,resourceName,subResourceName,params) {
+    clean: function(context,config,resourceName,subResourceName,params) {
 
         //do cleaning activity
-        //this.repeatForAll();
-    }
+        this.gotoSubResources();
+    },
 
-    function build(context,config,resourceName,subResourceName,params) {
+    build: function(context,config,resourceName,subResourceName,params) {
 
-    }
+        this.gotoSubResources();
+    },
 
-    function deploy(context,config,resourceName,subResourceName,params) {
+    deploy: function(context,config,resourceName,subResourceName,params) {
+        console.log('base deploy')
+        this.gotoSubResources();
+    },
 
-    }
 
-
-    function prompt(context, config) {
+    prompt: function(context, config) {
         //check is there inputParams for the specific config
         //check whether all inputParams are found in context.getVariable(); if not prompt and store the input value in the variables (setVariable())
 
     }
 }
+
+exports.baseAdapter 			= baseAdapter;
--- context.js
+++ context.js
@@ -51,10 +51,6 @@ function context(config, env) {
             return variables[vName];
     }
 
-    this.getConfig = function() {
-        return config;
-    }
-
     //setVariable; set the variable in the context
 
     this.setVariable = function(name, value) {
@@ -83,7 +79,7 @@ function context(config, env) {
                 return null;
             }
         } else {
-            return undefined;
+            return this.config;
         }
     }
 
@@ -93,3 +89,102 @@ function context(config, env) {
 }
 
 exports.getContext = getContext;
+
+
+/*
+
+
+var yaml                            = require('js-yaml');
+var fs                              = require('fs');
+var path                            = require('path');
+
+var instance;
+
+function getContext(config, env) {
+
+    if (!instance) {
+        instance                    = new context(config, env);
+    }
+    return instance;
+}
+
+
+function context(config, env) {
+    this.variables = {};
+    this.env = env;
+    this.config = {};
+
+    //TODO:if object assign as it is; if text, load (find file type and load accordingly) and assign
+    if (typeof config === 'string' || config instanceof String) {
+        var configObj = null;
+
+        try {
+            var current_dir = process.cwd();
+            var config_file_path = path.join(current_dir, config)
+            configObj = yaml.safeLoad(fs.readFileSync(config_file_path, 'utf8'));
+        } catch (e) {
+            console.log('ERROR reading config file');
+        }
+
+        this.config = configObj;
+
+    } else {
+        this.config = config;
+    }
+}
+
+context.prototype = {
+    getVariable: function (variableName) {
+        // variables[variableName] if not found return undefined
+        if (!variablenName) return undefined;
+
+        var vName = variableName.replace("$","").replace("\'","");
+
+        if (variables[vName]) return undefined;
+        else
+            return variables[vName];
+    },
+
+    getConfig: function() {
+        return this.config;
+    },
+
+    //setVariable; set the variable in the context
+
+    setVariable: function(name, value) {
+        variables[name] = value;
+    },
+
+    getConfig: function (resourceName, subResourceName) {
+        if (resourceName) {
+            var resource = this.config[resourceName];
+            if (resource) {
+
+                var resourceType = resource.type;
+                if (subResourceName) {
+                    var subResource = resource.subResources[subResourceName];
+                    if (subResource) {
+                        subResource.resourceName = resourceName;
+                        subResource.resourceType = resourceType;
+                        return subResource;
+                    }else {
+                        return null;
+                    }
+                } else {
+                    return resource;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return undefined;
+        }
+    },
+
+    getEnvironment: function() {
+        return env;
+    }
+}
+
+exports.getContext = getContext;
+ */
