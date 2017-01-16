a2c0a3effb7e3a5b446277376faf322448cbda1d
Gautham V Kidiyoor <gauthamvk@gauthamvk-macbookpro.roam.corp.google.com>
2017-01-16 13:21:37 +0530

    exporting gulp

--- gulpfile.js
+++ gulpfile.js
@@ -19,57 +19,56 @@ var manager
 // TODO: p1 index.js
 // TODO: org, data source
 
-
-gulp.task('init', function() {
-    if (argv.env)
-        env             = argv.env
-
-    if (argv.config)
-        config_file     = argv.config
-
-    print('ENV : ' + env)
-    print('CONIG_FILE : ' + config_file)
-
-    context             = context_builder.getContext(config_file, env)
-    manager             = manager_builder.getManager()
-
-    // set arguments passed to context
-    var args_passed     = argv
-    delete args_passed['_']
-    delete args_passed['$0']
-
-    for (var i=0; i<Object.keys(args_passed).length; i++) {
-        context.setVariable(Object.keys(args_passed)[i], args_passed[Object.keys(args_passed)[i]])
-    }
-});
-
-gulp.task('clean', ['init'], function(cb){
-    params = {}
-    manager.doTask('CLEAN', context, argv.resource ,argv.subresource, params, function () {
-        cb()
-    })
-});
-
-gulp.task('build',['init','clean'], function(cb){
-    params = {};
-
-    manager.doTask('BUILD', context, argv.resource ,argv.subresource, params, function () {
-        cb()
-    })
-});
-
-
-gulp.task('deploy', ['init','build'], function(cb){
-    params = {}
-    manager.doTask('DEPLOY', context, argv.resource ,argv.subresource, params, function () {
-        cb()
-    })
-});
-
-
-
 function print(msg) {
     gutil.log(gutil.colors.green(msg));
 }
 
 
+module.exports = function(gulp){
+
+    gulp.task('init', function() {
+        if (argv.env)
+            env             = argv.env
+
+        if (argv.config)
+            config_file     = argv.config
+
+        print('ENV : ' + env)
+        print('CONIG_FILE : ' + config_file)
+
+        context             = context_builder.getContext(config_file, env)
+        manager             = manager_builder.getManager()
+
+        // set arguments passed to context
+        var args_passed     = argv
+        delete args_passed['_']
+        delete args_passed['$0']
+
+        for (var i=0; i<Object.keys(args_passed).length; i++) {
+            context.setVariable(Object.keys(args_passed)[i], args_passed[Object.keys(args_passed)[i]])
+        }
+    });
+
+    gulp.task('clean', ['init'], function(cb){
+        params = {}
+        manager.doTask('CLEAN', context, argv.resource ,argv.subresource, params, function () {
+            cb()
+        })
+    });
+
+    gulp.task('build',['init','clean'], function(cb){
+        params = {};
+
+        manager.doTask('BUILD', context, argv.resource ,argv.subresource, params, function () {
+            cb()
+        })
+    });
+
+
+    gulp.task('deploy', ['init','build'], function(cb){
+        params = {}
+        manager.doTask('DEPLOY', context, argv.resource ,argv.subresource, params, function () {
+            cb()
+        })
+    });
+};
